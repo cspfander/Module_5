4f4972dcaea6267f7f4975aebdae5fd0973c574a
packe <cspfander@dmacc.edu>
2019-09-23 18:53:32 -0500

    Working Code - Corrected for loop range decrements and added useful comments

--- looping/basic_for_loops.py
+++ looping/basic_for_loops.py
@@ -13,8 +13,10 @@ loop to print the odd number descending from 99 to 33
 if __name__ == '__main__':
     numbers = [3.14, 24.0, 11.22, 1.01]
 
+    # for loop to print all numbers in list numbers
     for num in numbers:
         print(num)
 
-    for odd_num in range(99, 32, -3):
+    # prints all odd numbers from 99 to 33
+    for odd_num in range(99, 32, -2):
         print(odd_num)
