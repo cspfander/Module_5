6acd971689a3e78edc8261d3275a86d79f1f1bac
packe <cspfander@dmacc.edu>
2019-09-23 19:45:37 -0500

    Test Passed, average() function working correctly

--- test/test_loop.py
+++ test/test_loop.py
@@ -2,6 +2,7 @@ import unittest
 from input_loops import average_input_scores
 
 
+# tests the function average()
 class MyTestCase(unittest.TestCase):
     def test_average(self):
         self.assertEqual(85, average_input_scores.average([85, 90, 80]))
