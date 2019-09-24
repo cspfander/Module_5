d5b66e55e807571584330823016de02e41fc67ae
packe <cspfander@dmacc.edu>
2019-09-23 19:31:25 -0500

    Initial Commit

---
+++ input_loops/average_input_scores.py
@@ -0,0 +1,13 @@
+"""
+Program: average_input_scores.py
+Author: Colten Pfander
+Last date modified: 9/23/2019
+
+
+
+The purpose of this program is to read in one person's names, first and last, their age, and multiple scores
+out of 100, storing them in a list
+"""
+
+
+if __name__ == '__main__':
