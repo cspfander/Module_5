0fe2f552642f0e7d8c6d7445af2b0f06a88bdce1
packe <cspfander@dmacc.edu>
2019-09-23 20:12:28 -0500

    Added Try / Except block, while loop with sentinel for accepting inputs for scores as well as useful comments

--- input_loops/average_input_scores.py
+++ input_loops/average_input_scores.py
@@ -10,7 +10,7 @@ out of 100, storing them in a list
 """
 
 
-def average(score_list):
+def average(score_list):  # function to average a list of stored inputs
     total_score = 0
     num_of_scores = len(score_list)
     for score in score_list:
@@ -20,4 +20,22 @@ def average(score_list):
 
 
 if __name__ == '__main__':
-    average()
+    try:
+        scores = []
+        input_score_message = "Please enter a test score, or enter -1 to end the program: "  # sentinel value / flag + getting score
+        last_name = input("Please enter your last name: ")
+        first_name = input("Please enter your first name: ")
+        test_score = float(input(input_score_message))
+
+        while test_score >= 0:
+            scores.append(test_score)
+            test_score = float(input(input_score_message))
+        avg_scores = average(scores)
+        print(last_name + ", " + first_name + " average grade: %.2f" % avg_scores)
+    except ValueError:
+        print("Error! Please use only numbers for test scores!")  # try / except block to catch letters being put into scores
+
+    # tested inputs 1: Pfander, Colten, 100, 50, -1
+    # outputs 1: Pfander, Colten average grade: 75.00
+    # tested inputs 2: Sandler, Adam, 25, 25, 100, 100, 90, -1
+    # outputs 2: Sandler, Adam average grade: 68.00
